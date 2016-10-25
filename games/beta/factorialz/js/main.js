4d9ddb8aee4ec93378ed795a5bfd76f9ffef422f
Luke Bousfield <lukebousfield@Lukes-MacBook-Pro.local>
2016-10-24 20:30:51 -0600

    Added expanding mode

--- games/beta/factorialz/js/main.js
+++ games/beta/factorialz/js/main.js
@@ -36,6 +36,8 @@ $('#const1').focus();
 
 var op1plus = true;
 var op2plus = true;
+var op1plusexp = true;
+var op2plusexp = true;
 
 function swOp(num, isPlus) {
     if (num === 1) {
@@ -55,19 +57,50 @@ function swOp(num, isPlus) {
     }
 }
 
-$('#const1').keydown(function (event) {
+function swOpExp(num, isPlus) {
+    if (num === 1) {
+        op1plusexp = isPlus;
+        if (op1plusexp) {
+            $('#op1exp').text('+');
+        } else {
+            $('#op1exp').text('-');
+        }
+    } else {
+        op2plusexp = isPlus;
+        if (op2plusexp) {
+            $('#op2exp').text('+');
+        } else {
+            $('#op2exp').text('-');
+        }
+    }
+}
+
+$('#const1').keydown(function(event) {
     if (event.keyCode === 17) {
         swOp(1, !op1plus);
     }
 });
 
-$('#const2').keydown(function (event) {
+$('#const2').keydown(function(event) {
     if (event.keyCode === 17) {
         swOp(2, !op2plus);
     }
 });
 
-$('#answerForm-factor').submit(function(event) {
+$('#mid').keydown(function(event) {
+    if (event.keyCode === 17) {
+        console.log('switching');
+        swOpExp(1, !op1plusexp);
+    }
+})
+
+$('#const').keydown(function(event) {
+    if (event.keyCode === 17) {
+        swOpExp(2, !op2plusexp);
+    }
+});
+
+$('#answerForm-factor, #answerForm-expand').submit(function(event) {
 
     if (type === 'factor') {
 
@@ -100,11 +133,56 @@ $('#answerForm-factor').submit(function(event) {
         swOp(1, true);
         swOp(2, true);
 
+    } else {
+
+        event.preventDefault();
+
+        var squareCo = $('#squareco').val() || 1;
+        var middleTerm = $('#mid').val() || 0;
+        var constTerm = $('#const').val() || 0;
+
+        console.log(squareCo);
+
+        squareCo = parseInt(squareCo);
+        middleTerm = parseInt(middleTerm);
+        constTerm = parseInt(constTerm);
+
+        if (!op1plusexp) {
+            console.log('switching middle term');
+            middleTerm = -middleTerm;
+        }
+        if (!op2plusexp) {
+            console.log('switching constant term');
+            constTerm = -constTerm;
+        }
+
+        var theirAnswer = {
+            squareCo: squareCo,
+            middleTerm: middleTerm,
+            constTerm: constTerm
+        };
+
+        isCorrect(theirAnswer, answer);
+
+        $('#squareco').val('');
+        $('#mid').val('');
+        $('#const').val('');
+
+        var isCoAllowed = $('#allowCos').prop('checked');
+
+        if (isCoAllowed) return $('#squareco').focus();
+
+        $('#mid').focus();
+
+        swOpExp(1, true);
+        swOpExp(2, true);
+
     }
 
 });
 
 function isCorrect(theirAnswer, correctAnswer) {
+    if (type === 'expand') console.log(theirAnswer, correctAnswer);
     if (theirAnswer.squareCo === correctAnswer.squareCo && theirAnswer.constTerm === correctAnswer.constTerm && theirAnswer.middleTerm === correctAnswer.middleTerm) {
         alert('Correct!');
         displayProblem();
@@ -134,10 +212,10 @@ $('#factor').click(function() {
    displayProblem();
 });
 
-/* $('#expand').click(function() {
+$('#expand').click(function() {
     type = 'expand';
     displayProblem();
-}); For now */
+});
 
 $('#allowCos').click(function () {
     displayProblem();
@@ -186,22 +264,6 @@ function generateProblem() {
 
         var problem = squareCoefficient + 'x^2+' + middleTerm + 'x+' + constant;
 
-        problem = problem.replace(/\+-/g, '-');
-        problem = problem.replace(/\+1x/g, '+x');
-        problem = problem.replace(/-1x/g, '-x');
-        problem = problem.replace(/\+0x/g, '');
-        problem = problem.replace(/-0x/g, '');
-        problem = problem.replace('1x^2', 'x^2');
-
-
-        return {
-            problem: problem,
-            answer: {
-                squareCo: squareCoefficient,
-                middleTerm: middleTerm,
-                constTerm: constant
-            }
-        };
 
     } else {
 
@@ -210,8 +272,32 @@ function generateProblem() {
         var const1 = num1;
         var const2 = num2;
         var problem = '(' + co1 + 'x+' + const1 + ')(' + co2 + 'x+' + const2 + ')';
+        var answer = {
+            squareCo: squareCoefficient,
+            middleTerm: middleTerm,
+            constTerm: constant
+        };
 
+        problem = problem.replace(/\(1x/g, '(x');
+        problem = problem.replace(/\+0/g, '');
+        problem = problem.replace(/-0/g, '');
     }
+
+    problem = problem.replace(/\+-/g, '-');
+    problem = problem.replace(/\+1x/g, '+x');
+    problem = problem.replace(/-1x/g, '-x');
+    problem = problem.replace(/\+0x/g, '');
+    problem = problem.replace(/-0x/g, '');
+    problem = problem.replace('1x^2', 'x^2');
+
+    return {
+        problem: problem,
+        answer: {
+            squareCo: squareCoefficient,
+            middleTerm: middleTerm,
+            constTerm: constant
+        }
+    };
 
 }
 
@@ -224,7 +310,31 @@ function displayProblem() {
 
 
     var probElem = $('#problem');
-    probElem.text('Factor: ' + problem);
+    if (type === 'factor') {
+        var isCoAllowed = $('#allowCos').prop('checked');
+
+        if (!isCoAllowed) {
+            $('#squareco1').hide();
+            $('#squareco2').hide();
+        } else {
+            $('#squareco1').show();
+            $('#squareco2').show();
+        }
+        probElem.text('Factor: ' + problem);
+        $('#answerForm-factor').show();
+        $('#answerForm-expand').hide();
+    } else {
+        var isCoAllowed = $('#allowCos').prop('checked');
+
+        if (!isCoAllowed) {
+            $('#squareco').hide();
+        } else {
+            $('#squareco').show();
+        }
+        probElem.text('Expand:' + problem);
+        $('#answerForm-expand').show();
+        $('#answerForm-factor').hide();
+    }
 
     var isCoAllowed = $('#allowCos').prop('checked');
 
